b50e153d2e8405dadb871e23898b3ecb0357ef06
Fahim Mahdi <fmb.mondal@gmail.com>
2024-06-25 22:38:05 +0600

    Starting to recap the REACT core concepts

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h2>React Core Concepts</h2>
+      
+    </>
+  )
+}
+
+export default App
